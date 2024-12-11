53f4dde31a9526e8b2b7a5433a217d16cbd3e4bd
MatteoB23 <anycos2323@gmail.com>
2024-12-11 09:47:33 +0100

    Create and delete s3 bucket

---
+++ s3Bucket.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+aws s3 mb s3://m3461234567890
+
+# Check if the bucket was created
+if [ $? -eq 0 ]; then
+    echo "S3 Bucket 'm3461234567890' erfolgreich erstellt."
+else
+    echo "S3 Bucket konnte nicht erstellt werden"
+fi
+ 
+ # Remove content of the bucket
+aws s3 rm s3://m3461234567890 --recursive
+
+# Delete the bucket
+aws s3 rb s3://m3461234567890
+
+# Check if the bucket was deleted
+if [ $? -eq 0 ]; then
+    echo "S3 Bucket 'm3461234567890' erfolgreich gelöscht."
+else
+    echo "S3 Bucket konnte nicht gelöscht werden"
+fi
